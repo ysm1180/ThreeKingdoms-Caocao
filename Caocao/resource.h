a8d60dedcdd60aa6140dd1eab4653fa86d7fb01b
Yeon SeongMin <ysm1180@gmail.com>
2021-08-01 00:03:51 +0900

    Rename file and namespace

--- Caocao/resource.h
+++ Caocao/resource.h
@@ -1,12 +1,12 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// jojogame.rc에서 사용되고 있습니다.
+// caocao.rc에서 사용되고 있습니다.
 //
 #define IDD_LUACONSOLE                  101
 #define IDC_LOG                         1001
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
